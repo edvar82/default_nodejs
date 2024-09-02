741b27e27a26a94fa7a89287db9e58702c39637d
Edvar Monteiro <emsn@ic.ufal.br>
2024-09-02 09:24:13 -0300

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
+// app.use(cors({
+//     origin: ['https://example.com', 'https://another-example.com']
+// }));  
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
